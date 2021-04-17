2a32de48b3dc4ef687f5326345ee78d2bd3d69d1
Khadijah Flowers <chase_hatter@berkeley.edu>
2021-04-16 20:53:32 -0700

    Update kids-with-the-greatest-number-of-candies.java

--- kids-with-the-greatest-number-of-candies.java
+++ kids-with-the-greatest-number-of-candies.java
@@ -1,5 +1,7 @@
-class Solution {
-    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+import java.util.*;
+
+public class Solution {
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int max = 0;
         
         for (int i = 0; i < candies.length; i++) {
@@ -8,15 +10,24 @@ class Solution {
             }
         }
         
-        List<Boolean> extraCandy = new Arraylist<>();
+        List<Boolean> extraCandy = new ArrayList<>();
         for (int j = 0; j < candies.length; j++) {
-            int sum = extraCandies + candies[i];
-            if (sum > max) {
+            int sum = extraCandies + candies[j];
+            if (sum >= max) {
                 extraCandy.add(true);
             } else {
                 extraCandy.add(false);
             }
         }
         return extraCandy;
+    }
+    
+    public static void main(String[] args) {
+        int[] candy = new int[] {2, 3, 5, 1, 3};
+        List<Boolean> lis = Solution.kidsWithCandies(candy, 3);
+        
+        for (int y = 0; y < candy.length; y++) {
+            System.out.println(lis.get(y));
+        }
     }
 }
